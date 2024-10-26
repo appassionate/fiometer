aa971dcedc9054d17c91ee71ea25662f6627f028
appassionate <xiongke@stu.xmu.edu.cn>
2024-10-26 21:57:49 +0800

    imple: pydantic for serialize and task record

--- fioer/input.py
+++ fioer/input.py
@@ -1,14 +1,15 @@
+from pydantic import BaseModel
 from .utils import ini2dict, dict2ini
 
-class Input():
+class FioInput(BaseModel):
     
-    content:dict = {}
-
+    content:dict={}
+    
     def from_input_file(self, filename):
         
         with open(filename, 'r') as f:
             self.content = ini2dict(f.read())
-                    
+    
     
     def render_dict(self):
         

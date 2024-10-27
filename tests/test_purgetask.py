64f6064cfc8718713c206255cf4a966017e55379
appassionate <xiongke@stu.xmu.edu.cn>
2024-10-27 21:58:18 +0800

    update: purge test

--- tests/test_purgetask.py
+++ tests/test_purgetask.py
@@ -11,6 +11,37 @@ _WORK_PATH = '/tmp/fioer/test/'
 _TEMPLATE_DIR = Path(fioer.__file__).joinpath('../../templates').resolve()
 
 
-#test fioer.job.FioTask
 
-# class TestPurgeTask(unittest.TestCase)
+class TestPurgeTask(unittest.TestCase):
+    
+    def test_purgetask_init(self):
+        """purge task init(create) test"""
+        
+        _purge = PurgeTask(
+            work_path=_WORK_PATH+"test_purge_init",
+            input_dict={},
+        )
+        _purge.input.content["device"] = "/dev/XXXXXXXXXX"
+        _purge.write_input()
+        
+        # file exists
+        self.assertTrue(Path(_purge.get_file_directory("input.json")).exists())
+        self.assertTrue(Path(_purge.get_file_directory("output")).exists())
+        
+    def test_purgetask_run(self):
+        """ purge task run test"""
+        
+        _purge = PurgeTask(
+            work_path=_WORK_PATH+"test_purge_run",
+            input_dict={},
+        )
+        
+        _purge.run()
+        
+        
+        # file exists
+        self.assertTrue(Path(_purge.get_file_directory("input.json")).exists())
+        self.assertTrue(Path(_purge.get_file_directory("output")).exists())
+        # _purge._load_workpath()
+        # self.assertTrue(_purge.status == "done")
+        

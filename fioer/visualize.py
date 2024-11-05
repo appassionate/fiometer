28318bb2066acc8f037688b45036a9d26dc5f346
appassionate <xiongke@stu.xmu.edu.cn>
2024-11-06 00:01:44 +0800

    feat: filter output jobs by job name

--- fioer/visualize.py
+++ fioer/visualize.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import copy
 
 from pydantic import BaseModel
 import numpy as np
@@ -13,6 +14,9 @@ logger = get_logger(__name__)
 
 class ViewCache():
     data = {}
+    def _clean(self):
+        self.data = {}
+    
 #singleton for cache output, avoid pydantic lock
 vcache = ViewCache()    
 
@@ -33,20 +37,18 @@ def _create_fig_both():
     fig, axs = plt.subplots(2, 1, figsize=(10, 2 * 2), dpi=_DPI, gridspec_kw={'hspace': 0.7})
     return fig, axs
 
-def _draw_jobnum_text(ax, job_num):
+def _draw_jobnum_text(ax, job_name, job_num, total_num):
     
     # add text : top-left, 
-    # ax.set_title(f"job: No.{job_num}", loc="left", fontsize=12, color="black")
+    ax.set_title(f"job: {job_name}, No. {job_num}/{total_num}", loc="left", fontsize=12, color="black")
     pass
 
 def get_job_names(output):
     jobnames = [job["jobname"] for job in output[0]["jobs"]]
     return list(set(jobnames))
 
-def filter_job_by_name(output, jobname):
-    jobs = [job for job in output[0]["jobs"] if job["jobname"] == jobname]
-    return jobs
 
+#currently output and view are coupled
 class FioView(BaseModel):
     
     
@@ -77,8 +79,21 @@ class FioView(BaseModel):
         output = self.output
         return get_job_names(output)
     
+    def filter_output_by_name(self, job_name=None):
+        
+        if not job_name:
+            job_name = self.job_names[0]
+            logger.info(f"job_name not specified, using the first job name: {job_name}")
+        # TODO: deepcopy will be slow
+        _output = copy.deepcopy(self.output)
+        for frame in _output:
+            frame["jobs"] = [job for job in frame["jobs"] if job["jobname"] == job_name]
+
+        return _output
+    
+    
     #TODO: can view the by jobname and jobnum
-    def view_latency(self, mode="write", lat_type="lat", job_num=0, ax=None):
+    def view_latency(self, mode="write", lat_type="lat", job_name=None, job_num=0, ax=None):
         """_summary_
 
         Args:
@@ -101,6 +116,7 @@ class FioView(BaseModel):
             return fig, axs
         
         output = self.output
+        output = self.filter_output_by_name(job_name)
         
         if lat_type not in LATENCY_TYPE:
             raise ValueError(f"latency type not supported: {lat_type}")
@@ -140,11 +156,11 @@ class FioView(BaseModel):
         #grid setting: "--"
         ax.grid(True, linestyle='--')
         #add extra text
-        _draw_jobnum_text(ax, job_num)
+        # _draw_jobnum_text(ax, job_num)
         
         return fig, ax
 
-    def view_iops(self, mode, job_num=0, ax=None):
+    def view_iops(self, mode, job_name=None, job_num=0, ax=None):
         """ view one job iops over frames
 
         Args:
@@ -158,15 +174,16 @@ class FioView(BaseModel):
         
         if mode=="both":
             fig, axs = _create_fig_both()
-            self.view_iops(mode="write", job_num=job_num, ax=axs[0])
-            self.view_iops(mode="read", job_num=job_num, ax=axs[1])
+            self.view_iops(mode="write", job_name=None, job_num=job_num, ax=axs[0])
+            self.view_iops(mode="read",  job_name=None, job_num=job_num, ax=axs[1])
             return fig, axs
         
         output = self.output
+        output = self.filter_output_by_name(job_name)
         lenframe = len(output)
 
-        collect_iops_mean = [frame["jobs"][0][mode]["iops"] for frame in output]
-        collect_iops = [frame["jobs"][0][mode]["iops_mean"] for frame in output]
+        collect_iops_mean = [frame["jobs"][job_num][mode]["iops"] for frame in output]
+        collect_iops = [frame["jobs"][job_num][mode]["iops_mean"] for frame in output]
 
         # traj[0]["jobs"][0]["write"]["iops_min"]
         # traj[0]["jobs"][0]["write"]["iops_max"]
@@ -187,8 +204,7 @@ class FioView(BaseModel):
             _color = "r"
         # plot iops
         ax.plot(range(len(output)), collect_iops, label="iops", color=_color)
-        ax.plot(range(len(output)), collect_iops_mean,
-        label="iops_mean", color=_color, alpha=0.2)
+        ax.plot(range(len(output)), collect_iops_mean, label="iops_mean", color=_color, alpha=0.2)
 
         # add title and labels
         ax.set_title(f"{mode.upper()}: IOPS (IOs/sec) Over {lenframe} Frames")
@@ -206,11 +222,11 @@ class FioView(BaseModel):
         ax.grid(True, linestyle='--')
 
         #add extra text
-        _draw_jobnum_text(ax, job_num)
+        # _draw_jobnum_text(ax, job_num)
 
         return fig, ax
 
-    def view_bw(self, mode, job_num=0, ax=None):
+    def view_bw(self, mode, job_name=None, job_num=0, ax=None):
         """ view one job bandwidth over frames
 
         Args:
@@ -226,11 +242,12 @@ class FioView(BaseModel):
         
         if mode=="both":
             fig, axs = _create_fig_both()
-            self.view_bw(mode="write", job_num=job_num, ax=axs[0])
-            self.view_bw(mode="read", job_num=job_num, ax=axs[1])
+            self.view_bw(mode="write", job_name=None, job_num=job_num, ax=axs[0])
+            self.view_bw(mode="read",  job_name=None, job_num=job_num, ax=axs[1])
             return fig, axs
         
         output = self.output
+        output = self.filter_output_by_name(job_name)
         lenframe = len(output)
 
         collect_bw_mean = [frame["jobs"][0][mode]["bw"] for frame in output]
@@ -268,12 +285,12 @@ class FioView(BaseModel):
         ax.grid(True, linestyle='--')
 
         #add extra text
-        _draw_jobnum_text(ax, job_num)
+        #_draw_jobnum_text(ax, job_num)
 
         return fig, ax
 
 
-    def view_iops_bw(self, mode, job_num=0, ax=None):
+    def view_iops_bw(self, mode, job_name=None, job_num=0, ax=None):
         """ view one job iops and bandwidth over frames
 
         Args:
@@ -287,62 +304,62 @@ class FioView(BaseModel):
         
         # fig, ax
         fig, ax = plt.subplots(1, 2, figsize=(10, 3), dpi=_DPI, gridspec_kw={'hspace': 1,"wspace": 0.3}) 
-        self.view_bw(mode=mode, job_num=job_num, ax=ax[0])
-        self.view_iops(mode=mode, job_num=job_num, ax=ax[1])
+        self.view_bw(mode=mode, job_name=job_name, job_num=job_num, ax=ax[0])
+        self.view_iops(mode=mode, job_name=job_name, job_num=job_num, ax=ax[1])
 
         return fig, ax
         
         
         
+    # #BUG: job_name not implemented
+    # def overview_latency(self, mode="write", lat_type="lat", dpi=300):
+    #     """overview latency of all jobs
 
-    def overview_latency(self, mode="write", lat_type="lat", dpi=300):
-        """overview latency of all jobs
+    #     Args:
+    #         mode (_type_): write/read/randwrite/randread
+    #         job_num (int, optional): which job to view. Defaults to index: 0
+    #         dpi (int, optional): _description_. Defaults to 300.
 
-        Args:
-            mode (_type_): write/read/randwrite/randread
-            job_num (int, optional): which job to view. Defaults to index: 0
-            dpi (int, optional): _description_. Defaults to 300.
+    #     Returns:
+    #         _type_: fig, ax of matplotlib 
+    #     """        
 
-        Returns:
-            _type_: fig, ax of matplotlib 
-        """        
+    #     # graph: overview, contains n*1 sub-graphs
+    #     output = self.output
+    #     job_num = len(output[0]["jobs"])
+    #     fig, axs = plt.subplots(job_num, 1, figsize=(10, 2 * job_num), dpi=_DPI, gridspec_kw={'hspace': 1})
 
-        # graph: overview, contains n*1 sub-graphs
-        output = self.output
-        job_num = len(output[0]["jobs"])
-        fig, axs = plt.subplots(job_num, 1, figsize=(10, 2 * job_num), dpi=_DPI, gridspec_kw={'hspace': 1})
+    #     # avoiding subscriptable error
+    #     if job_num == 1:
+    #         axs = [axs]
+    #     for i, ax in enumerate(axs):
+    #         self.view_latency(mode=mode, lat_type=lat_type, job_num=i, ax=ax)
 
-        # avoiding subscriptable error
-        if job_num == 1:
-            axs = [axs]
-        for i, ax in enumerate(axs):
-            self.view_latency(mode=mode, lat_type=lat_type, job_num=i, ax=ax)
-
-        return fig, axs
+    #     return fig, axs
     
     
-    def overview_iops(self, mode="write", dpi=300):
-        """overview iops of all jobs
+    # def overview_iops(self, mode="write", dpi=300):
+    #     """overview iops of all jobs
 
-        Args:
-            mode (_type_): write/read/randwrite/randread
-            dpi (int, optional): _description_. Defaults to 300.
+    #     Args:
+    #         mode (_type_): write/read/randwrite/randread
+    #         dpi (int, optional): _description_. Defaults to 300.
 
-        Returns:
-            _type_: fig, ax of matplotlib
-        """        
+    #     Returns:
+    #         _type_: fig, ax of matplotlib
+    #     """        
         
-        # TODO: dpi imple
+    #     # TODO: dpi imple
 
-        # graph: overview, contains n*1 sub-graphs
-        output = self.output
-        job_num = len(output[0]["jobs"])
-        fig, axs = plt.subplots(job_num, 1, figsize=(10, 2 * job_num), dpi=_DPI, gridspec_kw={'hspace': 1})
+    #     # graph: overview, contains n*1 sub-graphs
+    #     output = self.output
+    #     job_num = len(output[0]["jobs"])
+    #     fig, axs = plt.subplots(job_num, 1, figsize=(10, 2 * job_num), dpi=_DPI, gridspec_kw={'hspace': 1})
 
-        # avoiding subscriptable error
-        if job_num == 1:
-            axs = [axs]
-        for i, ax in enumerate(axs):
-            self.view_iops(mode=mode, job_num=i, ax=ax)
+    #     # avoiding subscriptable error
+    #     if job_num == 1:
+    #         axs = [axs]
+    #     for i, ax in enumerate(axs):
+    #         self.view_iops(mode=mode, job_num=i, ax=ax)
 
-        return fig, axs
+    #     return fig, axs

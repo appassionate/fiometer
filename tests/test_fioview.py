bb0cd8b161490af047e128051b2001a4ff2e3a24
appassionate <xiongke@stu.xmu.edu.cn>
2024-11-07 22:51:36 +0800

    feat: add test fioview

--- tests/test_fioview.py
+++ tests/test_fioview.py
@@ -5,6 +5,29 @@ import fioer
 
 _WORK_PATH = '/tmp/fioer/test/'
 _TEMPLATE_DIR = Path(fioer.__file__).joinpath('../../templates').resolve()
-_SAMPLE_DIR = Path(fioer.__file__).joinpath('../../samples').resolve()
+_SAMPLE_DIR = Path(fioer.__file__).joinpath('../../tests/samples').resolve()
+
+
+
+@pytest.fixture
+def setup_fiotask():
+    _fio = FioTask(
+        work_path=_WORK_PATH+"randrw65",
+        input_dict={},
+    )
+    return _fio
+
 
 #@pytest.mark.parametrize
+def test_fioview_view(setup_fiotask):
+    
+    _fio = setup_fiotask
+
+    _fio.view.view_iops(mode="write", job_name="file1", job_num=0, ax=None)
+    _fio.view.view_iops(mode="read",  job_name="file1", job_num=0, ax=None)
+
+    _fio.view.view_latency(mode="write", job_name="file1", job_num=0, ax=None)
+    _fio.view.view_latency(mode="read",  job_name="file1", job_num=0, ax=None)
+
+    _fio.view.view_iops_bw(mode="write",  job_name="file1", job_num=0, ax=None)
+    _fio.view.view_iops_bw(mode="read",   job_name="file1", job_num=0, ax=None)

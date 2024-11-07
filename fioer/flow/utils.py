4d4d68b49bff0ac3cffcfb8683825dc69928d035
appassionate <xiongke@stu.xmu.edu.cn>
2024-11-07 23:35:34 +0800

    update: add a demo func to check iops convergence

---
+++ fioer/flow/utils.py
@@ -0,0 +1,10 @@
+from fioer.job import FioTask
+from typing import List
+
+def check_tasks_convergence(tasks: List[FioTask], threshold: float = 0.01) -> bool:
+    """
+    check tests convergence: steady state or not
+    """
+    #TODO 
+    
+    pass

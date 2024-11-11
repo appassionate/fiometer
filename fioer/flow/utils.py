b6fd4df41dd341ec96e4f9db14a5fd739a03e160
appassionate <xiongke@stu.xmu.edu.cn>
2024-11-11 22:43:36 +0800

    add abstraction converge

--- fioer/flow/utils.py
+++ fioer/flow/utils.py
@@ -1,6 +1,21 @@
 from fioer.job import FioTask
 from typing import List
 
+
+
+def calc_test_slope(data: List[float]) -> float:
+    """
+    calculate the slope of the data
+    """
+    
+    
+    
+
+
+
+
+
+
 def check_tasks_convergence(tasks: List[FioTask], threshold: float = 0.01) -> bool:
     """
     check tests convergence: steady state or not
@@ -8,3 +23,5 @@ def check_tasks_convergence(tasks: List[FioTask], threshold: float = 0.01) -> bo
     #TODO 
     
     pass
+
+
